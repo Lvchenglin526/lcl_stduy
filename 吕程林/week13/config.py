b0c860c87175db869fe5a435cecf5d420f3a038a
Lvchenglin526 <l13225645371@163.com>
2026-01-22 18:42:05 +0800

    Add files via upload

---
+++ 吕程林/week13/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "model_output",
+    "schema_path": "ner_data/schema.json",
+    "train_data_path": "ner_data/train",
+    "valid_data_path": "ner_data/test",
+    "vocab_path": "chars.txt",
+    "max_length": 100,
+    "hidden_size": 256,
+    "num_layers": 2,
+    "epoch": 20,
+    "batch_size": 16,
+    "tuning_tactics": "lora_tuning",
+    "pooling_style": "max",
+    "seed": 987,
+    "optimizer": "adam",
+    "learning_rate": 1e-4,
+    "use_crf": False,
+    "class_num": 9,
+    "pretrain_model_path": r"D:\ANACONDA\envs\py312\Lib\site-packages\modelscope\hub\models\google-bert\bert-base-chinese"
+}
+
